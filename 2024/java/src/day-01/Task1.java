d1cfec99bbe75ef40cddb256554dd6200749f755
Zhivko Kocev <zikekocev57@gmail.com>
2024-12-01 16:45:22 +0100

    Added ts solutions

---
+++ 2024/java/src/day-01/Task1.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Task1 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> first = new ArrayList<>();
+        List<Integer> second = new ArrayList<>();
+        br.lines().map(line -> line.split("\\s+")).forEach(pair -> {
+            first.add(Integer.parseInt(pair[0]));
+            second.add(Integer.parseInt(pair[1]));
+        });
+
+        first.sort(Comparator.naturalOrder());
+        second.sort(Comparator.naturalOrder());
+
+        int sum = 0;
+        for (int i = 0; i < first.size(); i++) {
+            sum += Math.abs(first.get(i) - second.get(i));
+        }
+
+        System.out.println(sum);
+    }
+}
---
+++ 2024/java/src/day-01/Task2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Task2 {
+
+    private static long countAppearance(List<Integer> list, int number) {
+        return list.stream().filter(num -> num == number).count();
+    }
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> first = new ArrayList<>();
+        List<Integer> second = new ArrayList<>();
+        br.lines().map(line -> line.split("\\s+")).forEach(pair -> {
+            first.add(Integer.parseInt(pair[0]));
+            second.add(Integer.parseInt(pair[1]));
+        });
+
+        first.sort(Comparator.naturalOrder());
+        second.sort(Comparator.naturalOrder());
+
+        int sum = 0;
+        for (int i = 0; i < first.size(); i++) {
+            sum += countAppearance(second, first.get(i)) * first.get(i);
+        }
+
+        System.out.println(sum);
+
+    }
+}
