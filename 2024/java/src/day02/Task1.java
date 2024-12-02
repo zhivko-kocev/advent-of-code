fafa766909e5d831e3360eb77b22ab64a11dba96
Zhivko Kocev <zikekocev57@gmail.com>
2024-12-02 11:08:38 +0100

    Day 02 - ref

--- 2024/java/src/day02/Task1.java
+++ 2024/java/src/day02/Task1.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiPredicate;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
